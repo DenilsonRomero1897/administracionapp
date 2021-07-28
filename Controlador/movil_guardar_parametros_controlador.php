a423dc4644179c3c872de99251f0d8d5a07b7d9a
paola120819 <kelinglr@gmail.com>
2021-07-27 23:21:45 -0600

    up

--- Controlador/movil_guardar_parametros_controlador.php
+++ Controlador/movil_guardar_parametros_controlador.php
@@ -6,12 +6,12 @@ require_once('../clases/funcion_bitacora_movil.php');
 
 
 $Id_objeto = 128;
-$opcion = $_GET['op'];
+$opcion = isset($_GET["op"]) ? ($_GET["op"]) : "";
 
 
 if ($opcion == 'eliminar') {
-  $id_parametro= isset($_GET["id"]) ? ($_GET["id"]) : "";
-  $sql = "DELETE FROM tbl_movil_parametros WHERE id = $id_parametro";
+  $id_parametros= isset($_GET["id"]) ? ($_GET["id"]) : "";
+  $sql = "DELETE FROM tbl_movil_parametros WHERE id = $id_parametros";
   bitacora_movil::evento_bitacora($_SESSION['id_usuario'], $Id_objeto, 'elimino', "$sql");
   $resultado = $mysqli->query($sql);
   if($resultado === TRUE){
@@ -40,51 +40,39 @@ if ($opcion == 'eliminar') {
                   </script>';
           }
 }elseif ($opcion == 'editar') {
-  $id_parametro = isset($_GET["id"]) ? ($_GET["id"]) : "";
-  $parametro = isset($_POST["parametro"]) ? strtoupper($_POST["parametro"]) : "";
+  //funcionalidad para ejecutar el update
+  $id_parametros= isset($_GET["id"]) ? ($_GET["id"]) : "";
+  $parametro= isset($_POST["parametro"]) ? strtoupper($_POST["parametro"]) : "";
   $descripcion = isset($_POST["descripcion"]) ? strtoupper($_POST["descripcion"]) : "";
-  $valor=isset($_POST["valor"]) ? strtoupper($_POST["valor"]) : "";
-  $sql="UPDATE tbl_movil_parametros  set parametro= '$parametro', descripcion= '$descripcion', valor ='$valor' WHERE id = $id_parametro";
-  bitacora_movil::evento_bitacora($_SESSION['id_usuario'], $Id_objeto, 'modifico', 'EL PARAMETRO' . $parametro. '');
+  $valor = isset($_POST["valor"]) ? strtoupper($_POST["valor"]) : "";
+  $sql="UPDATE tbl_movil_parametros set parametro= '$parametro', descripcion= '$descripcion', valor=  '$valor' , WHERE id = $id_parametros";
+  bitacora_movil::evento_bitacora($_SESSION['id_usuario'], $Id_objeto, 'modifico', 'EL PARAMETRO' . $id_parametros. '');
   $mysqli->query($sql);
   header('location: ../vistas/movil_gestion_parametros_vista.php?msj=2');
 }else{
+  //se almacenan los valores para realizar el insert
   $parametro = isset($_POST["parametro"]) ? strtoupper($_POST["parametro"]) : "";
   $descripcion = isset($_POST["descripcion"]) ? strtoupper($_POST["descripcion"]) : "";
-  $valor =isset($_POST["valor"]) ? strtoupper($_POST["valor"]) : "";
+  $valor = isset($_POST["valor"]) ? strtoupper($_POST["valor"]) : "";
+  $fecha_modificacion= isset($_POST["txt_fecha_modificacion"]) ? $_POST["txt_fecha_modificacion"] : "";
+  $modificadopor = isset($_SESSION['id_usuario']) ? ($_SESSION['id_usuario']) : "";
+  $usuario_id = isset($_SESSION['id_usuario']) ? ($_SESSION['id_usuario']) : "";
   
-
-
-///Logica para el que se repite
-$sqlexiste = ("select count(parametro) as parametro from tbl_movil_parametros where parametro='$parametro'");
-//Obtener la fila del query
-$existe = mysqli_fetch_assoc($mysqli->query($sqlexiste));
-
-
-
 /* Logica para que no acepte campos vacios */
-if ($_POST['parametro']  ) {
-
-  /* Condicion para que no se repita el rol*/
-  if ($existe['parametro'] == 1) {
-    //redireccion ya que el nombre segmento existe
- 	header('location: ../vistas/movil_gestion_parametros_vista.php?msj=1'); 
-    
-  } else {
-
+if ($_POST['parametro'] <>  ' ' and  $_POST['descripcion'] <>' 'and  $_POST['valor'] <> '' ) {
     /* Query para que haga el insert*/
-    $sql = "INSERT into tbl_movil_parametros (id,parametro,descripcion,valor,fecha_modificacion, modificador por, usuario_id)   VALUES (null,'$parametro',$descripcion','$valor', $modificadopor', $usuario_id', sysdate())";
+    $sql = "INSERT into tbl_movil_parametros VALUES (null,'$parametro', '$descripcion', '$valor','$fecha_modificacion' , '$modificadopor', '$usuario_id' )";
+    
     $resultado = $mysqli->query($sql);
-
-
+ 
     if ($resultado) {
-      bitacora_movil::evento_bitacora($_SESSION['id_usuario'], $Id_objeto, 'inserto', "$sql");
+      bitacora_movil::evento_bitacora($_SESSION['id_usuario'], $Id_objeto, 'inserto',strtoupper("$sql"));
 
       header('location: ../vistas/movil_gestion_parametros_vista.php?msj=2');
     } else {
       echo "Error: " . $sql;
     }
-  }
+  
 } else {
   header('location: ../vistas/movil_gestion_parametros_vista.php?msj=3');
 }
