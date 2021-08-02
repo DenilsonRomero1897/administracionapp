835b6d8a421ac42aa46da16578276ed7dd7f0754
Riccy27 <82992231+Riccy27@users.noreply.github.com>
2021-08-02 00:37:55 -0600

    update bitacora segmentos

--- Controlador/movil_reporte_bitacora.php
+++ Controlador/movil_reporte_bitacora.php
@@ -24,10 +24,10 @@ class myPDF extends FPDF
         $this->Cell(330, 10, utf8_decode("DEPARTAMENTO DE INFORMÁTICA "), 0, 0, 'C');
         $this->ln(10);
         $this->SetFont('times', 'B', 20);
-        $this->Cell(330, 10, utf8_decode("REPORTE GESTION BITACORA"), 0, 0, 'C');
+        $this->Cell(330, 10, utf8_decode("REPORTE GESTIÓN BITÁCORA"), 0, 0, 'C');
         $this->ln(17);
         $this->SetFont('Arial', '', 12);
-        $this->Cell(60, 10, utf8_decode("GESTIONES EXISTENTES"), 0, 0, 'C');
+        $this->Cell(100, 10, utf8_decode(""), 0, 0, 'C');
         $this->Cell(420, 10, "FECHA: " . $fecha, 0, 0, 'C');
         $this->ln();
     }
@@ -42,27 +42,39 @@ class myPDF extends FPDF
     {
         $this->SetFont('Times', 'B', 12);
         $this->SetLineWidth(0.3);
-        $this->Cell(10, 7, "ID", 1, 0, 'C');
-        $this->Cell(30, 7, utf8_decode("USUARIO_ID"), 1, 0, 'C');
-        $this->Cell(30, 7, utf8_decode("OBJETO_ID"), 1, 0, 'C');
-        $this->Cell(30, 7, "ACCION", 1, 0, 'C');
-        $this->Cell(200, 7, "DESCRIPCION", 1, 0, 'C');
+        $this->Cell(10, 7, utf8_decode("N°"), 1, 0, 'C');
+        $this->Cell(30, 7, utf8_decode("USUARIO"), 1, 0, 'C');
+        $this->Cell(30, 7, utf8_decode("OBJETO"), 1, 0, 'C');
+        $this->Cell(30, 7, utf8_decode("ACCIÓN"), 1, 0, 'C');
+        $this->Cell(200, 7, utf8_decode("DESCRIPCIÓN"), 1, 0, 'C');
         $this->Cell(40, 7, "FECHA", 1, 0, 'C');
 
         $this->ln();
     }
     function viewTable()
-    {
+    {  
         global $instancia_conexion;
-        $sql = "select * FROM tbl_movil_bitacoras";
+        $sql = "
+        select  
+            n.id,
+            s.Usuario,
+            p.objeto,
+            n.accion,
+            n.descripcion,
+            n.fecha
+            
+           
+        FROM
+            tbl_movil_bitacoras n inner join tbl_usuarios s on n.usuario_id=s.Id_usuario
+            inner join tbl_objetos p on n.objeto_id=p.id_objeto ";
+       
         $stmt = $instancia_conexion->ejecutarConsulta($sql);
-
-        while ($reg = $stmt->fetch_array(MYSQLI_ASSOC)) {
+         while ($reg = $stmt->fetch_array(MYSQLI_ASSOC)) {
 
             $this->SetFont('Times', '', 12);
             $this->Cell(10, 7, $reg['id'], 1, 0, 'C');
-            $this->Cell(30, 7, utf8_decode($reg['usuario_id']), 1, 0, 'C');
-            $this->Cell(30, 7, utf8_decode($reg['objeto_id']), 1, 0, 'C');
+            $this->Cell(30, 7, utf8_decode($reg['Usuario']), 1, 0, 'C');
+            $this->Cell(30, 7, utf8_decode($reg['objeto']), 1, 0, 'C');
             $this->Cell(30, 7, $reg['accion'], 1, 0, 'C');
             $this->Cell(200, 7, $reg['descripcion'], 1, 0, 'C');
             $this->Cell(40, 7, $reg['fecha'], 1, 0, 'C');
