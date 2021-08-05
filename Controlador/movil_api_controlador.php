ba02be0d939846fc9e60f703e1450fa96e721a59
Isai Romero <isaibatres1897@gmail.com>
2021-08-05 01:18:40 -0600

    Revert "up"
    This reverts commit dc947e5a1439b56b1845336d556178000aa76eb8.

--- Controlador/movil_api_controlador.php
+++ Controlador/movil_api_controlador.php
@@ -13,23 +13,18 @@
 
 //peticion post
 function consumoApi($url, $datos){
-        $ch = curl_init();
+$ch = curl_init();
                    
 $data = json_encode($datos);
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_POST,true);
-curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
-curl_setopt($ch,CURLOPT_HTTPHEADER,array('Content-Type:application/json'));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 $response = curl_exec($ch);
-if (curl_errno($ch)){
-     echo curl_error($ch);
-}else{
-     $decoded = json_decode($response, true);
-}
+if (curl_errno($ch)) echo curl_error($ch);
+else $decoded = json_decode($response, true);
 curl_close($ch);
 return $decoded;
-    
 
 }
 
