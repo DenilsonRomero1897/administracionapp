d997d7e1d63ec41e220cf5df2f412d1bc134f57f
Riccy27 <82992231+Riccy27@users.noreply.github.com>
2021-08-14 01:22:51 -0600

    update reportes

--- vistas/movil_gestion_parametros_vista.php
+++ vistas/movil_gestion_parametros_vista.php
@@ -173,12 +173,12 @@ ob_end_flush();
     <!--Pantalla 2-->
     <div class="card card-default">
       <div class="card-header">
-        <div class="card-tools">
-          <a class="btn btn-primary btn-xs" href="../vistas/movil_crear_parametros_vista.php">Nuevo</a>
-          <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+      <div class="dt-buttons btn-group"><button class="btn btn-secondary buttons-pdf buttons-html5 btn-danger" tabindex="0" aria-controls="tabla2" type="button" id="GenerarReporte" title="Exportar a PDF"><span><i class="fas fa-file-pdf"></i> </span> </button> </div>
+      <a class="btn btn-primary btn-xs float-right" href="../vistas/movil_crear_parametros_vista.php">Nuevo</a>
+        <!--buscador-->
+        <div class="float-right mt-5 ml-5">
+        <input class="form-control" placeholder="Buscar..." type="text" id="buscartext" name="buscar" onpaste="return false" onkeyup="leer(this.value)">
         </div>
-        <div class="dt-buttons btn-group"><button class="btn btn-secondary buttons-pdf buttons-html5 btn-danger" tabindex="0" aria-controls="tabla2" type="button" id= "GenerarReporte"  title="Exportar a PDF"><span><i class="fas fa-file-pdf"></i> </span> </button> </div>
-     
       <!-- /.card-header -->
       <div class="card-body">
         <table id="tabla" class="table table-bordered table-striped">
@@ -270,7 +270,7 @@ ob_end_flush();
       $('#tabla').DataTable({
         "paging": true,
         "lengthChange": true,
-        "searching": true,
+        "searching": false,
         "ordering": true,
         "info": true,
         "autoWidth": true,
