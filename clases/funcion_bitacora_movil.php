e4a5c22aa4bbe62696bf8c60b54197eddc787666
Isai Romero <79403229+DenilsonRomero1897@users.noreply.github.com>
2021-08-15 23:32:42 -0600

    Update funcion_bitacora_movil.php

--- clases/funcion_bitacora_movil.php
+++ clases/funcion_bitacora_movil.php
@@ -8,7 +8,6 @@
 			    $descripcion2 = $mysqli->escape_string($descripcion);
 			   	$sql_bitacora = "INSERT INTO  tbl_movil_bitacora (usuario_id, objeto_id, accion , descripcion , fecha)
     			 VALUES ('$usuario_id', '$objeto_id' , '$accion', '$descripcion2' , sysdate())";
-		        var_dump($sql_bitacora); 
 			$mysqli->query($sql_bitacora);
 		}
 		
